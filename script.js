8ed41cb61be1022b9c5611688cced2df71480cd2
OlovesH <kholeif64@gmail.com>
2024-05-13 15:13:42 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 document.getElementById('myButton').addEventListener('click', function() {
-    document.getElementById('message').innerText = 'i love you 7bobi  donia ^ donia ';
+    document.getElementById('message').innerText = '💖 i love you 7bobi  donia ^ donia 💖';
 });
